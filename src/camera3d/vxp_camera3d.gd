36f7631e049be8d75f8b338722d3f75422d579cb
0xab7a3ffab8 <0xab7a3ffab8@gmail.com>
2024-02-22 17:17:08 +0100

    Added some things to vxpCamera3D and Commented its script.

--- src/camera3d/vxp_camera3d.gd
+++ src/camera3d/vxp_camera3d.gd
@@ -5,9 +5,13 @@ class_name vxpCamera3D
 @export var mouse_rotation:bool = true
 @export var mouse_sens:Vector2 = Vector2(0.01, 0.01)
 @export var capture_mode:Input.MouseMode = Input.MOUSE_MODE_CAPTURED:
-	set(new):
+	set(new): # INFO: Use the set function so that the mouse mode get updated.
 		Input.mouse_mode = new
 		capture_mode = new
+
+@export_category("JoyStick")
+@export var joystick_rotation:bool = false
+@export var joystick_sens:Vector2 = Vector2(0.01, 0.01)
 
 @export_category("RotationClamp")
 @export var max_yaw:float = PI
@@ -16,7 +20,7 @@ class_name vxpCamera3D
 @export var max_pitch:float = 0.5*PI
 @export var min_pitch:float = -0.5*PI
 
-@export var lock_yaw:bool = false
+@export var lock_yaw:bool = false # TODO: Insert super funny Joke here.
 @export var lock_pitch:bool = true
 
 @export_category("RotationObjects")
@@ -32,10 +36,10 @@ func camara_view_dir_y_only()->Vector3:
 
 
 func rotate_with_camara(vec:Vector3)->Vector3:
-	return rotate_with_camera_y(vec).rotated(Vector3.LEFT, pitch_object.rotation.y)
+	return rotate_with_camera_y(vec).rotated(Vector3.LEFT, pitch_object.rotation.y) # There is most likely a more efficient way of dooing this.
 
 func camera_view_dir()->Vector3:
-	return rotate_with_camara(Vector3.FORWARD)
+	return rotate_with_camara(Vector3.FORWARD) # There is most likely a more efficient way of dooing this.
 
 
 func _ready() -> void:
@@ -43,9 +47,13 @@ func _ready() -> void:
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		if mouse_rotation:
+		if mouse_rotation: # WARNING: This is not resolution Independent (yet).
 			yaw_object.rotate_y(-event.relative.x * mouse_sens.x)
 			pitch_object.rotate_x(-event.relative.y * mouse_sens.y)
+		if joystick_rotation:
+			pass # TODO: Implement this.
 
 		if lock_pitch:
 			pitch_object.rotation.x = min(max(pitch_object.rotation.x, min_pitch), max_pitch)
+		if lock_yaw:
+			yaw_object.rotation.y = min(max(yaw_object.rotation.y, min_yaw), max_yaw)
